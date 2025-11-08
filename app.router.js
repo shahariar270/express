33de127c8fce1b757747a3c87dadb01d279a78d8
shahariar270 <mdshahariar102@gmail.com>
2025-11-08 08:27:38 +0600

    added route post

--- app.router.js
+++ app.router.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const products = require('./ecommerse.products');
+let products = require('./ecommerse.products');
 const app = express();
 
 app.route('/books')
     .get((req, res) => {
         res.status(200).json({
             message: 'fetch all data',
+            products,
+        })
+    })
+    .post((req, res) => {
+        const updateProduct = res.body;
+        products = { ...products, updateProduct }
+        res.status(201).json({
+            message: 'data added success',
             products,
         })
 
