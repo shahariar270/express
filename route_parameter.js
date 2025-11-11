c0a7511c0104ef78d089efc08948db877d1c3bda
shahariar270 <mdshahariar102@gmail.com>
2025-11-11 22:00:50 +0600

    fix: add GET route for books by ID with response handling

--- route_parameter.js
+++ route_parameter.js
@@ -2,4 +2,19 @@ const express = require('express');
 const app = express();
 let products = require('./ecommerse.products');
 
+app.get('/books/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = products.find((p) => p.id === id);
+    if (data) {
+        return res.status(200).json({
+            message: 'data founded',
+            data,
+        })
+    }
+
+    return res.status(404).json({
+        massage: 'not data match founded'
+    })
+})
+
 module.exports = app;
