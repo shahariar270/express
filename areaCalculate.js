abfc86c6c83284d3b71470c55781d85b089c4eb7
shahariar270 <mdshahariar102@gmail.com>
2025-11-12 21:35:04 +0600

    code improved

--- areaCalculate.js
+++ areaCalculate.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-let products = require('./ecommerse.products');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,12 +10,19 @@ app.get('/area', (req, res) => {
 })
 
 app.post('/area', (req, res) => {
-    const length = req.body.length;
-    const width = req.body.width;
-    let calculateArea = width * length;
+    const { length, width } = req.body;
+    let calculateArea = Number(width) * Number(length);
+    if (!length || !width) {
+        res.status(400).json({
+            message: 'Please provide both length and width',
+            code: 400,
+        })
+        return;
+    }
     res.status(200).json({
         message: ' your Rectangle Area is',
         calculateArea,
+        code: 200,
 
     })
 })
