029edf3c6bbacd5f7eee3a693512e61ab81ec779
shahariar270 <mdshahariar102@gmail.com>
2025-11-24 06:54:44 +0600

    data

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const app = require('./middleware');
 
-app.listen(port, () => {
-    console.log(`server is running in http://localhost:${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`server is running in http://localhost:${port}`);
+// })
--- middleware.js
+++ middleware.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.get('/middle', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './public/middle.html'));
+const auth = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        res.status(401).json({
+            message: 'unauthorization token'
+        })
+    }
+    next();
+}
+
+app.get('/profile', auth, (req, res) => {
+    res.status(200).json({
+        message: 'data fetch successfully'
+    })
 })
 
-module.exports = app;
+app.listen(port, () => {
+    console.log('run server in port 8080');
+})
