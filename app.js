d9d7758199979e9fe43b10ad76264155ea25d2e1
shahariar270 <mdshahariar102@gmail.com>
2025-12-13 20:04:24 +0600

    margan run successfully

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
+const express = require('express');
 const port = process.env.PORT || 3000;
 const { default: chalk } = require('chalk');
 const app = require('./basic.app');
+const morgan = require('morgan');
+const products = require('./ecommerse.products');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        massage: 'sussesfully Data render',
+        products,
+    })
+})
+
 
 app.listen(port, () => {
-    console.log(chalk.green(`server is running in http://localhost:${port}`));
+    console.log(chalk.blue(`server is running in http://localhost:${port}`));
 })
--- basic.app.js
+++ basic.app.js
@@ -2,13 +2,5 @@ const express = require('express');
 const products = require('./ecommerse.products');
 const app = express();
 
-app.use(express.json())
-
-app.get('/', (res, req) => {
-    req.status(200).json({
-        massage: 'sussesfully Data render',
-        products,
-    })
-})
 
 module.exports = app;
