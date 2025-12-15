bcb7ee7a0311b2828f5d9fb7443fe381dcf95032
shahariar270 <mdshahariar102@gmail.com>
2025-12-15 21:55:37 +0600

    inital setup

---
+++ validations/app.js
@@ -0,0 +1,22 @@
+const { default: chalk } = require('chalk');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        massage: 'you hit test endpoint'
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(chalk.blue.bold.italic(`server ruing at ${port}`));
+    // console.log(chalk.rgb('render successfully'));
+})
