b40cc300d45a036f913ced79f7f9398743f461cb
shahariar270 <mdshahariar102@gmail.com>
2025-12-15 22:22:54 +0600

    create post request

--- validations/router/router.js
+++ validations/router/router.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 router.post('/user', (req, res) => {
     try {
-        
+        const id = Date.name();
+        const { name, email, password, dob } = req.body;
+        if (!name && !email, !password) {
+            res.status(400).json({
+                massage: `missing some payload`
+            })
+        }
+        const newUser = { id, name, email, dob };
+        res.status(201).json({
+            massage: 'new user created successfully',
+            data: newUser
+        })
+
     } catch (error) {
-        
+        console.log(error);
     }
 })
