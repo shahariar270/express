2b00c81d67c3610f2472d15d0f5a759277e6bcca
shahariar270 <mdshahariar102@gmail.com>
2025-12-16 16:41:47 +0600

    issue fixed

---
+++ validations/errorHandle/auth_validator/register_validator.js
@@ -0,0 +1,22 @@
+const { body } = require('express-validator');
+
+exports.register_validator = [
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name is required'),
+
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format'),
+
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters'),
+];
+
--- validations/router/router.js
+++ validations/router/router.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const RegisterSchema = require('../config/register_config');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
 const validation_request = require('../errorHandle/validate_request');
+const {register_validator} = require('../errorHandle/auth_validator/register_validator');
 
 router.post('/register',
-    body('name').trim().notEmpty().withMessage('Name is require'),
-    body('email').trim().notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
-    validation_request(),
+    register_validator,
+    validation_request,
     async (req, res) => {
         try {
             const id = Date.now();
             const { name, email, password, dob } = req.body;
 
-            if (!name || !email || !password) {
-                return res.status(400).json({
-                    message: 'name, email or password is missing'
-                });
-            }
             const newUser = { id, name, email, password, dob };
             const user = await RegisterSchema.create(newUser)
             return res.status(201).json({
