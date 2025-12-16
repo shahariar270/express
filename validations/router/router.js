cfa2a8e53cc5472b70d86c882de7444a1bf080dc
shahariar270 <mdshahariar102@gmail.com>
2025-12-16 08:42:00 +0600

    added daata try catch block

--- validations/router/router.js
+++ validations/router/router.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/user', (req, res) => {
-    const id = Date.now();
-    const { name, email, password, dob } = req.body || {};
+router.post('/user', async(req, res) => {
+    try {
+        const id = Date.now();
+        const { name, email, password, dob } = req.body || {};
 
-    if (!name || !email || !password) {
-        return res.status(400).json({
-            message: 'name, email or password is missing'
-        });
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                message: 'name, email or password is missing'
+            });
+        }
+        const newUser = { id, name, email, dob };
+        return res.status(201).json({
+            massage: 'new user created successfully',
+            data: newUser
+        })
+    } catch (error) {
+
     }
-    const newUser = { id, name, email, dob };
-    return res.status(201).json({
-        massage: 'new user created successfully',
-        data: newUser
-    })
 })
 
 module.exports = router;
