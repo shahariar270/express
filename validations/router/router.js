d6a96e251e2ceb0eddf79e1b2c09b736d27fd3dd
shahariar270 <mdshahariar102@gmail.com>
2025-12-16 15:54:42 +0600

    validation added

--- validations/router/router.js
+++ validations/router/router.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const RegisterSchema = require('../config/register_config');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/register', async (req, res) => {
-    try {
-        const id = Date.now();
-        const { name, email, password, dob } = req.body;
-
-        if (!name || !email || !password) {
+router.post('/register',
+    body('name').trim().notEmpty().withMessage('Name is require'),
+    body('email').trim().notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
             return res.status(400).json({
-                message: 'name, email or password is missing'
+                message: 'Validation failed',
+                errors: errors.array()
             });
         }
-        const newUser = { id, name, email, password, dob };
-        const user = await RegisterSchema.create(newUser)
-        return res.status(201).json({
-            massage: 'new user created successfully',
-            data: user
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message
-        })
-    }
-})
+        try {
+            const id = Date.now();
+            const { name, email, password, dob } = req.body;
+
+            if (!name || !email || !password) {
+                return res.status(400).json({
+                    message: 'name, email or password is missing'
+                });
+            }
+            const newUser = { id, name, email, password, dob };
+            const user = await RegisterSchema.create(newUser)
+            return res.status(201).json({
+                massage: 'new user created successfully',
+                data: user
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message
+            })
+        }
+    })
 
 module.exports = router;
